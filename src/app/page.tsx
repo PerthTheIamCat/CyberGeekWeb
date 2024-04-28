7fc827ab42df2f9e36fd7fcc8e07b8d6c475f3a2
zjeee <wiritipon.t@ku.th>
2024-04-28 20:03:32 +0700

    add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { GoRelFilePath } from "react-icons/go";
+import { GoAlertFill } from "react-icons/go";
+
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,57 @@
 import React from 'react'
+import { GoSignIn } from "react-icons/go";
+
 
 const navbar = () => {
   return (
     <>
-<nav className="bg-zinc-800 shadow shadow-gray-300 w-100 px-8 md:px-auto">
-	<div className="md:h-16 h-28 mx-auto md:px-4 flex items-center justify-between flex-wrap md:flex-nowrap">
-		<div className="text-indigo-500 md:order-1">
-			<div>LOGO</div>
+	<nav className="bg-black shadow shadow-gray-700 w-100 px-8 md:px-auto">
+		<div className="md:h-16 h-28 mx-auto md:px-4 flex items-center justify-between flex-wrap md:flex-nowrap">
+			<div className="text-white font-semibold font-kanit md:order-1 transition-all duration-250 hover:scale-110 ">
+					<div className='text-2xl px-4'>
+						CGC
+					</div>
+					<div className='text-xs'>
+						CyberGeekClub
+					</div>
+			</div>
+			<div className="text-white order-3 w-full md:w-auto md:order-2">
+				<ul className="flex font-semibold justify-between ">
+				
+					<li className="group md:px-4 md:py-2 transition-all duration-250 hover:scale-110 hover:text-green-400">
+						<a href="#">
+							สมัครเข้าชมรม
+							<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-400"></span>
+						</a>
+					</li>
+					<li className="group md:px-4 md:py-2 transition-all duration-250 hover:scale-110 hover:text-green-400">
+						<a href="#" >
+							เกี่ยวกับเรา
+							<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-400"></span>
+						</a>
+					</li>
+					<li className="group md:px-4 md:py-2 transition-all duration-250 hover:scale-110 hover:text-green-400">
+						<a href="#" >
+							ข่าวสาร
+							<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-400"></span>
+						</a>
+					</li>
+					<li className="group md:px-4 md:py-2 transition-all duration-250 hover:scale-110 hover:text-green-400">
+						<a href="#" >
+							ติตต่อเรา
+							<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-400"></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div className="order-2 md:order-3">
+				<button className="px-4 py-2   transition-all duration-250 hover:scale-110 hover:text-green-400 text-gray-50 rounded-xl flex items-center gap-2">
+					<GoSignIn className=' text-2xl'/>
+					<span>เข้าสู่ระบบ</span>
+				</button>
+			</div>
 		</div>
-		<div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
-			<ul className="flex font-semibold justify-between">
-				<li className="md:px-4 md:py-2 text-indigo-500"><a href="#">สมัครเข้าชมรม</a></li>
-				<li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">เกี่ยวกับเรา</a></li>
-				<li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">ข่าวสาร</a></li>
-				<li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">ติตต่อเรา</a></li>
-			</ul>
-		</div>
-		<div className="order-2 md:order-3">
-			<button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
-                <span>เข้าสู่ระบบ</span>
-            </button>
-		</div>
-	</div>
-</nav>
+	</nav>
    
     </>
   )
